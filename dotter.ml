b476b266614bb6071ba6aa24055c34ec946deb94
Igor Khotin <khotin@gmx.com>
2020-11-18 23:35:20 +0200

    [*] dotter ml plan update

--- dotter.ml
+++ dotter.ml
@@ -1,12 +1,15 @@
 > concept document
-> init project
-> ghost space
-> ghosts
+> .dot format readme
+> full-featured keyboard controls
+> tag for trics
 > tokens
-> dots
-> ghost tricks
-> dotspace panel
-> ghostspace panel
 > ghost control panel
 > dialog panel
 > debug panel?
+V ghosts
+V dots
+V ghost tricks
+V ghost space
+V ghostspace panel
+V dotspace panel
+V init project
